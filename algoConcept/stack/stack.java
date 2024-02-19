030d547c11c326d546baf9702207823bb5c77557
이채하 <chaeha@kyowon.co.kr>
2024-02-19 17:35:47 +0900

    Stack_20240219

---
+++ algoConcept/stack/stack.java
@@ -0,0 +1,27 @@
+package stack;
+
+import java.util.*;
+
+public class stack {
+
+    public static void main (String[] args) throws Exception {
+        Stack<String> stack = new Stack<String>();
+        System.out.println(stack.isEmpty() + "//" + stack.size());
+
+        stack.push("최지우");
+        stack.push("고소영");
+        stack.push("전지현");
+
+        System.out.println(stack.isEmpty() + "//" + stack.size());
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty() + "//" + stack.size());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty() + "//" + stack.size());
+//        System.out.println(stack.pop());
+
+    }
+
+
+}
