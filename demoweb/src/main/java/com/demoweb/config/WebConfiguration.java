16c2d63ee9679b6cf7f11f7a0c5e4271bc0279f1
이채하 <chaeha93@gmail.com>
2024-06-26 17:26:17 +0900

    feat: 기본 구조 설정

---
+++ demoweb/src/main/java/com/demoweb/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.demoweb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+//    @Override
+//    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/static/**")
+//                .addResourceLocations("/static/", "classpath:/static/");
+//    }
+
+}
